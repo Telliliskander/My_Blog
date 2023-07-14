0ad0a6f4810bd794908c2ed35e707e99b9116902
tuto <skander.tlili@enstab.ucar.tn>
2023-07-14 22:28:42 +0200

    First commit : the entire ready project locally

---
+++ mysite/blog/admin.py
@@ -0,0 +1,35 @@
+from django.contrib import admin
+from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
+
+# Old version of postadmin before adding the summernote the admin forms
+# class PostAdmin(admin.ModelAdmin):
+#     list_display = ('title', 'slug', 'status','created_on')
+#     list_filter = ("status", "created_on")
+#     search_fields = ['title', 'content']
+#     prepopulated_fields = {'slug': ('title',)}
+
+
+
+class PostAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'slug', 'status','created_on')
+    list_filter = ("status", "created_on")
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
+
+admin.site.register(Post, PostAdmin)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'active')
+    list_filter = ('active', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(active=True)
+
+
+
